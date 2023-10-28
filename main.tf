fab73d9f2edc18e0dd3d045df0651ff4c3e93239
Olatunji Olayinka <olatunji.weber@gmail.com>
2023-10-28 13:35:15 +0200

    Final Commit - Terraform initializes, distinct s3 buckets created, Terraform destroying successfully...

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   region = "us-west-2"
 }
-
 variable "event_themes" {
   default = ["adventure-tech", "nature-escape", "data-summit"]
 }
-
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.15.1"
@@ -14,15 +12,11 @@ module "s3-bucket" {
 
   bucket = "${var.event_themes[count.index]}-oo-bucket"
 
-  # # Set access control settings directly within the block
-  # acl = "private"
-
   # Disable public access block
   block_public_acls   = false
   block_public_policy = false
   ignore_public_acls  = false
 }
-
 output "bucket_names" {
   value = module.s3-bucket[*]
 }
