7fccd25b4a4316b5ea4e46f472d62697d86ce07c
Olatunji Olayinka <olatunji.weber@gmail.com>
2023-10-28 13:24:39 +0200

    Created the s3 buckets using modules...

--- main.tf
+++ main.tf
@@ -3,18 +3,26 @@ provider "aws" {
 }
 
 variable "event_themes" {
-  default = ["AdventureTech", "NatureEscape", "DataSummit"]
+  default = ["adventure-tech", "nature-escape", "data-summit"]
 }
 
-resource "aws_s3_bucket_acl" "event_buckets" {
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
   count = length(var.event_themes)
 
-  bucket = "${var.event_themes[count.index]}-OO-bucket"
+  bucket = "${var.event_themes[count.index]}-oo-bucket"
 
-  # Set access control settings directly within the block
-  acl = "private"
+  # # Set access control settings directly within the block
+  # acl = "private"
+
+  # Disable public access block
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
 }
 
 output "bucket_names" {
-  value = aws_s3_bucket_acl.event_buckets[*].bucket
+  value = module.s3-bucket[*]
 }
